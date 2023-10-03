6173e5393d6e85547258880582b47a21ab9f58f9
GeorgiZhisov <ivojisov@gmail.com>
2023-10-03 13:18:56 +0300

    concat name

---
+++ data/concat names/concat names/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace concat_names;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string name = Console.ReadLine();
+        string surname = Console.ReadLine();
+        string delimer = Console.ReadLine();
+
+
+        Console.WriteLine($"{name}{delimer}{surname}");
+
+    }
+}
+
