b071ece3f44c0bb9614af9d5fd5fcdf514ff2ce0
GeorgiZhisov <ivojisov@gmail.com>
2023-10-03 11:56:15 +0300

    exact sum of real numbers

---
+++ data/exact sum of real numbers/exact sum of real numbers/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace exact_sum_of_real_numbers;
+class Program
+{
+    static void Main(string[] args)
+    {
+        decimal n = decimal.Parse(Console.ReadLine());
+        decimal sum = 0;
+        
+        for (int i = 0; i< n; i++)
+        {
+            decimal number = decimal.Parse(Console.ReadLine());
+            sum += number;
+            
+            
+
+            
+        }
+
+        Console.WriteLine(sum);
+
+    }
+}
+
