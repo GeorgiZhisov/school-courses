255f7dd3749914cc31263ab8ad6cab6fefc4b0d6
GeorgiZhisov <ivojisov@gmail.com>
2023-10-03 13:30:28 +0300

    upper or lower

--- data/chars to string/chars to string/Program.cs
+++ data/chars to string/chars to string/Program.cs
@@ -3,7 +3,13 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        string output = string.Empty;
+        for(int i = 0; i < 3; i++)
+        {
+            char input = char.Parse(Console.ReadLine());
+            output += input;
+        }
+        Console.WriteLine(output);
     }
 }
 
---
+++ data/lower or upper/lower or upper/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+namespace uperlower;
+
+class Program
+{
+    static void Main()
+    {
+        
+        char input = Console.ReadLine()[0]; 
+        if (char.IsUpper(input))
+        {
+            Console.WriteLine($"upper-case.");
+        }
+        else if (char.IsLower(input))
+        {
+            Console.WriteLine($"lower-case.");
+        }
+
+    }
+}
