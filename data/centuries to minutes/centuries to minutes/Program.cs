6f6a18f192a0719be35602624824ae2487390949
GeorgiZhisov <ivojisov@gmail.com>
2023-10-03 12:10:32 +0300

    centuries to minutes

---
+++ data/centuries to minutes/centuries to minutes/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace centuries_to_minutes;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int centuries = int.Parse(Console.ReadLine());
+
+        int years = centuries * 100;
+        int days =(int) (years * 365.2422);
+        int hours = days * 24;
+        int minutes = hours * 60;
+
+        Console.WriteLine($"{centuries} centuries = {years:F0} years = {days:F0} days = {hours:F0} hours = {minutes:F0} minutes");
+    }
+}
+
