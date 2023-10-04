fcd84d281b04dd31efe526bf4b208d8e02321b77
GeorgiZhisov <ivojisov@gmail.com>
2023-10-04 14:12:07 +0300

    rounding numbers

---
+++ array lab/3rd/3rd/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace _3rd;
+class Program
+{
+    static void Main(string[] args)
+    {
+        decimal[] numbers = Console.ReadLine()
+            .Split("", StringSplitOptions.RemoveEmptyEntries)
+            .Select(decimal.Parse)
+            .ToArray();
+
+        for (int i = 0; i< numbers.Length; i++)
+        {
+            Console.WriteLine($"{numbers[i]} => {Math.Round(numbers[i])}");
+        }
+
+
+    }
+}
+
