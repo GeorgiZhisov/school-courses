4452b64410752e565def1688dc45a22d48454f46
GeorgiZhisov <ivojisov@gmail.com>
2023-10-09 16:15:02 +0300

    sum of chars

---
+++ array homework/sum of chars/sum of chars/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        char[] characters = new char[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            characters[i] = char.Parse(Console.ReadLine());
+        }
+
+        int totalSum = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            totalSum += (int)characters[i];
+        }
+
+        Console.WriteLine($"The sum equals: {totalSum}");
+    }
+}
