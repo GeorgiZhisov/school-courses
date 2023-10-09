769e697d42ad38df74dc93aabab75892c6438c9d
GeorgiZhisov <ivojisov@gmail.com>
2023-10-09 16:01:03 +0300

    sum digits

---
+++ array homework/sum digits/sum digits/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int number = int.Parse(Console.ReadLine());
+
+        int sum = 0;
+        string numberString = number.ToString();
+
+        for (int i = 0; i < numberString.Length; i++)
+        {
+            sum += int.Parse(numberString[i].ToString());
+        }
+
+        Console.WriteLine(sum);
+    }
+}
+
