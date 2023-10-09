d9dda7bc8c4fc13ee0e1d36655af2bcf8e334635
GeorgiZhisov <ivojisov@gmail.com>
2023-10-09 16:03:20 +0300

    elevator

---
+++ array homework/elevator/elevator/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int p = int.Parse(Console.ReadLine());
+
+        int[] people = new int[n];
+        for (int i = 0; i < n; i++)
+        {
+            people[i] = int.Parse(Console.ReadLine());
+        }
+
+        int totalCourses = 0;
+        int currentCount = 0;
+
+        for (int i = 0; i < people.Length; i++)
+        {
+            currentCount += people[i];
+
+            if (currentCount >= p)
+            {
+                totalCourses++;
+                currentCount = 0;
+            }
+        }
+
+        if (currentCount > 0)
+        {
+            totalCourses++;
+        }
+
+        Console.WriteLine(totalCourses);
+    }
+}
+
+
