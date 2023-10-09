16d5023c6b59d8f8f5ac66e4ac90e0b0eea6f89e
GeorgiZhisov <ivojisov@gmail.com>
2023-10-09 16:21:51 +0300

    water overflow

---
+++ array homework/water overflow/water overflow/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        int n = int.Parse(Console.ReadLine());
+        int tankCapacity = 255;
+        int[] quantities = new int[n];
+
+        for (int i = 0; i < n; i++)
+        {
+            quantities[i] = int.Parse(Console.ReadLine());
+        }
+
+        int totalLiters = 0;
+
+        for (int i = 0; i < n; i++)
+        {
+            if (totalLiters + quantities[i] > tankCapacity)
+            {
+                Console.WriteLine("Insufficient capacity!");
+            }
+            else
+            {
+                totalLiters += quantities[i];
+            }
+        }
+
+        Console.WriteLine(totalLiters);
+    }
+}
+
+
