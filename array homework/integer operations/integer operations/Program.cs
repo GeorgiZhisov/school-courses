866cb44da3e1643beefac4a30c2502357c97e6f1
GeorgiZhisov <ivojisov@gmail.com>
2023-10-09 15:57:10 +0300

    integer operations

---
+++ array homework/integer operations/integer operations/Program.cs
@@ -0,0 +1,22 @@
+﻿
+
+class Program
+{
+    static void Main()
+    {
+        
+        int[] numbers = new int[4];
+
+        
+        for (int i = 0; i < 4; i++)
+        {
+            numbers[i] = int.Parse(Console.ReadLine());
+        }
+
+       
+        int result = (numbers[0] + numbers[1]) / numbers[2] * numbers[3];
+
+        
+        Console.WriteLine(result);
+    }
+}
