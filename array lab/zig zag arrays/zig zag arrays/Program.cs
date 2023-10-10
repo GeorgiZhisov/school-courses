9cd523d2e6eac35cb3a1a23c51e376d5c04ad1d7
GeorgiZhisov <ivojisov@gmail.com>
2023-10-10 12:20:57 +0300

    zig zag arr

---
+++ array lab/zig zag arrays/zig zag arrays/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace zig_zag_arrays;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int n = int.Parse(Console.ReadLine());
+        int[] firstarr = new int[n];
+        int[] secondarr = new int[n];
+
+        for(int i = 1; i <= n; i++)
+        {
+            int[] arr = Console.ReadLine()
+                .Split(" ", StringSplitOptions.RemoveEmptyEntries)
+                .Select(int.Parse)
+                .ToArray();
+            if (i % 2 == 0)
+            {
+                secondarr[i - 1] = arr[0];
+                firstarr[i - 1] = arr[1];
+                
+            }
+            else
+            {
+                secondarr[i - 1] = arr[1];
+                firstarr[i - 1] = arr[0];
+            }
+
+           
+        }
+        Console.WriteLine(string.Join(" ", firstarr));
+        Console.WriteLine(string.Join(" ", secondarr));
+    }
+}
+
