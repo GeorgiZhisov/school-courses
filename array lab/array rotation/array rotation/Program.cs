758004356a3c0d7ba68b9558c060cd488ffdb784
GeorgiZhisov <ivojisov@gmail.com>
2023-10-11 11:22:41 +0300

    array rotation

--- array lab/array rotation/array rotation/Program.cs
+++ array lab/array rotation/array rotation/Program.cs
@@ -3,7 +3,25 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+
+        int[] arr = Console.ReadLine().
+            Split(" ", StringSplitOptions.RemoveEmptyEntries)
+            .Select(int.Parse)
+            .ToArray();
+
+        int n = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < n; i++)
+        {
+            for (int j = arr.Length - 1; j >= 0; i--)
+            {
+                int tmep = arr[arr.Length - 1];
+                arr[arr.Length - 1] = arr[j];
+                arr[j] = tmep;
+            }  
+
+        }
+        Console.WriteLine(string.Join(" ", arr));
     }
 }
 
