6b840760f592a484b3380ac9bf4dce6f9d4e88b6
GeorgiZhisov <ivojisov@gmail.com>
2023-10-11 12:07:34 +0300

    everything in common

---
+++ array test/everything in common/everything in common/Program.cs
@@ -0,0 +1,31 @@
+﻿namespace everything_in_common;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] arr1 = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+        int[] arr2 = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+        int sum = 0;
+        bool ifequal = false;
+
+        for (int i = 0; i < arr1.Length; i++)
+        {
+            if (arr1[i] != arr2[i])
+            {
+                Console.WriteLine("Arrays are not identical.");
+                Console.WriteLine($"Found difference at {i} index ");
+                break;
+            }
+            sum += arr1[i];
+            if(i == arr1.Length - 1 && i == arr2.Length - 1)
+            {
+                Console.WriteLine("Arrays are identical.");
+                Console.WriteLine($"Sum: {sum} ");
+            }
+        }    
+    
+
+    }
+
+}
+
