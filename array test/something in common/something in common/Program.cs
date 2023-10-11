8f6dfd8a6e2fae2149497ba720f3df7487b4bbfb
GeorgiZhisov <ivojisov@gmail.com>
2023-10-11 11:44:49 +0300

    something in common

---
+++ array test/something in common/something in common/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace something_in_common;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string[] arr = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+        string[] array = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).ToArray();
+
+        foreach(string commonword in arr)
+        {
+            foreach(string commonword2 in array)
+            {
+                if (commonword == commonword2)
+                {
+                    Console.Write(($"{commonword} "));
+                }
+            }
+        }
+    }
+}
+
