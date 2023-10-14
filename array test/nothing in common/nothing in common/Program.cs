d968f4dc64d9f5ed23330a2e64d44afdc574fe36
GeorgiZhisov <ivojisov@gmail.com>
2023-10-14 17:39:46 +0300

    arrayd

---
+++ array test/nothing in common/nothing in common/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace nothing_in_common;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] arr = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries).Select(int.Parse).ToArray();
+        int targetsum = int.Parse(Console.ReadLine());
+
+        for (int i = 0; i < arr.Length; i++)
+        {
+            for(int j = i+1 ; j < arr.Length; j++)
+            {
+                if (arr[i] + arr[j] == targetsum)
+                {
+                    Console.WriteLine($"{arr[i]} {arr[j]}");
+                }
+            }
+        }
+          
+    }
+}
+
---
+++ hangman project/hangman/hangman/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace hangman;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+
