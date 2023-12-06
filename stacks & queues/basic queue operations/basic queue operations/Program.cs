561eac71d5a465a7b5db390b059ea23983079937
GeorgiZhisov <ivojisov@gmail.com>
2023-12-06 13:19:48 +0200

    aaa

---
+++ stacks & queues/basic queue operations/basic queue operations/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace basic_queue_operations;
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        
+
+    }
+}
+
---
+++ stacks & queues/maximum minimum element/maximum minimum element/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace maximum_minimum_element;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        int a = input[1];
+        int b = input[2];
+
+
+        List<int> enqueueNum = Console.ReadLine().Split().Select(int.Parse).ToList();
+        enqueueNum.ForEach(b )
+
+     
+
+    }
+}
+
