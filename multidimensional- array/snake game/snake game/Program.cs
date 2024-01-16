32760c1e598e92756fde5a0a9684b4f659a387fe
GeorgiZhisov <ivojisov@gmail.com>
2024-01-16 11:08:50 +0200

    domashna rabota

--- multidimensional- array/snake game/snake game/Program.cs
+++ multidimensional- array/snake game/snake game/Program.cs
@@ -3,7 +3,37 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int[] input = Console.ReadLine().Split().Select(int.Parse).ToArray();
+    string snake = Console.ReadLine();
+    int snakePont = 0;
+
+    char[][] Path = new char[input[0]][];
+
+            for (int row = 0; row<input[0]; row++)
+            {
+                Path[row] = new char[input[1]];
+                for (int col = 0; col<input[1]; col++)
+                {
+                   Path[row][col] = snake[snakePont];
+
+                    if (snakePont == snake.Length - 1)
+                        snakePont = 0;
+                    else
+                        snakePont++;
+                }
+
+                if (row % 2 != 0)
+                    Path[row] = Path[row].Reverse().ToArray();
+            }
+
+            for (int i = 0; i < input[0]; i++)
+{
+    for (int j = 0; j < input[1]; j++)
+    {
+        Console.Write(Path[i][j]);
+    }
+    Console.WriteLine();
+}
+        }
     }
 }
-
---
+++ multidimensional- array/snake moves/snake moves/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace snake_moves;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+
