b6b3a5874a27d962410aa6e07440397e362271ef
GeorgiZhisov <ivojisov@gmail.com>
2024-01-16 13:34:04 +0200

    2ra

--- multidimensional- array/agains bugs/agains bugs/Program.cs
+++ multidimensional- array/agains bugs/agains bugs/Program.cs
@@ -1,9 +1,45 @@
 ﻿namespace agains_bugs;
 class Program
 {
-    static void Main(string[] args)
+    static async void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        int n = int.Parse(Console.ReadLine());
+
+        int[] positions = new int[n];
+        int[] sprays = new int[n];
+
+        string[] input = Console.ReadLine().Split();
+        for(int i = 0; i< n; i++)
+        {
+            positions[i] = int.Parse(input[i]);
+        }
+
+        int days = 2;
+        while (true)
+        {
+            bool plantDied = false;
+            for(int i =1; i<n; i++)
+            {
+                if (sprays[i] > sprays[i - 1])
+                {
+                    sprays[i] = sprays[i - 1];
+                    plantDied = true;
+                }
+            }
+
+            if (!plantDied)
+            {
+                break;
+            }
+
+            days++;
+
+        }
+
+
+        Console.WriteLine(days);
+
+
     }
 }
 
