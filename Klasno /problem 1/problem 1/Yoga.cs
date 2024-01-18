2c9f46ba6022fa430b2205241aba4492c8bda592
GeorgiZhisov <ivojisov@gmail.com>
2024-01-18 20:15:12 +0200

    kl

---
+++ Klasno /problem 1/problem 1/Yoga.cs
@@ -0,0 +1,23 @@
+﻿namespace problem_1;
+class Program
+{
+    static void Main(string[] args)
+    {
+        double[] priceInput = Console.ReadLine().Split().Select(double.Parse).ToArray();
+        double peopleCount = double.Parse(Console.ReadLine());
+
+        double Mats = (priceInput[2] * peopleCount) * 1.2; 
+
+        double Balls = priceInput[0] * peopleCount;
+
+
+        int freeBalls = (int)(peopleCount / 5);
+        Balls -= priceInput[1] * freeBalls;
+
+        double Blocks = priceInput[1] * (2 * peopleCount); 
+        double sum = Mats + Balls + Blocks;
+
+        Console.WriteLine($"{sum:F2}$ needed for equipment.");
+    }
+}
+
---
+++ Klasno /problem 2/problem 2/Road Safety.cs
@@ -0,0 +1,73 @@
+﻿namespace problem_2;
+class Program
+{
+    static void Main(string[] args)
+    {
+
+        int greenLight = int.Parse(Console.ReadLine());
+        int window = int.Parse(Console.ReadLine());
+
+        Queue<string> carsQueue = new Queue<string>();
+        int passedCars = 0;
+        bool accidents = false;
+
+      
+
+        while (true)
+        {
+
+            string Input = Console.ReadLine().ToUpper();
+
+            if (Input == "STOP")
+            {
+                break;
+            }
+
+            if (Input == "LIGHT")
+            {
+                int remainingGreenLight = greenLight;
+
+                while (remainingGreenLight > 0 && carsQueue.Count > 0)
+                {
+                    string Car = carsQueue.Dequeue();
+
+                    if (remainingGreenLight - Car.Length >= 0)
+                    {
+                        remainingGreenLight -= Car.Length;
+                        passedCars++;
+                    }
+                    else if (remainingGreenLight - Car.Length + window >= 0)
+                    {
+                        passedCars++;
+                        break;
+                    }
+                    else
+                    {
+                        char Char = Car[remainingGreenLight + window];
+                        Console.WriteLine($"Accident! {Car} was crushed at {Char}.");
+                        accidents = true;
+                        break;
+                    }
+                }
+            }
+            else
+            {
+                carsQueue.Enqueue(Input);
+            }
+
+            if (accidents)
+            {
+                break;
+            }
+        }
+
+
+        if (!accidents)
+        {
+            Console.WriteLine("Success!");
+            Console.WriteLine($"{passedCars} total cars are gone.");
+        }
+    }
+
+}
+      
