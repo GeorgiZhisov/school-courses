7b624ae63d376b8e8e9f6077a7698b052f4fe07f
GeorgiZhisov <ivojisov@gmail.com>
2024-03-06 11:55:14 +0200

    functional programmming

---
+++ functional programming/custom coparator/custom coparator/Program.cs
@@ -0,0 +1,34 @@
+﻿namespace custom_coparator;
+class Program
+{
+    static void Main(string[] args)
+    {
+        int[] arr = Console.ReadLine().Split().Select(int.Parse).ToArray();
+        Array.Sort(arr);
+
+        List<int> sortedList = new List<int>();
+        Func<int, bool> func = x => x % 2 == 0;
+
+        foreach(int num in arr)
+        {
+            if(func(num))
+            {
+                sortedList.Add(num);
+            }
+
+        }
+
+        foreach(int num in arr)
+        {
+            if (!func(num))
+            {
+                sortedList.Add(num);
+            }
+        }
+
+        //Console.WriteLine(string.Join(" ", sortedList));
+        Action<List<int>> print = x => Console.Write(string.Join(" ", x));
+        print(sortedList);
+    }
+}
+
