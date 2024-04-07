c9fde7d23e1793ed78f32dd817d3150b0719b04a
GeorgiZhisov <ivojisov@gmail.com>
2024-04-07 21:15:14 +0300

    project break

---
+++ regex/regex/regex/Program.cs
@@ -0,0 +1,19 @@
+﻿using System.Text.RegularExpressions;
+namespace regex;
+class Program
+{
+    static void Main(string[] args)
+    {
+        string regexPattern = @"\b[A-Z][a-z]+\s[A-Z][a-z]+\b";
+
+        string input = Console.ReadLine();
+
+        MatchCollection matches = Regex.Matches(input, regexPattern);
+
+        foreach (Match match in matches)
+        {
+            Console.WriteLine(match.Value);
+        }
+    }
+}
+    
---
+++ regex/softuni bar income/softuni bar income/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace softuni_bar_income;
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, World!");
+    }
+}
+
