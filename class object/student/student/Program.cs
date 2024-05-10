39ce1ef8a23bdebc21a8065a7047c3bfd950cf0c
GeorgiZhisov <ivojisov@gmail.com>
2024-05-10 12:40:41 +0300

    aaa

--- class object/student/student/Program.cs
+++ class object/student/student/Program.cs
@@ -1,28 +1,51 @@
-﻿namespace student;
+﻿using System;
+using System.Collections.Generic;
 class Program
 {
     static void Main(string[] args)
     {
         int n = int.Parse(Console.ReadLine());
-        string[] name = Console.ReadLine().Split(" ", StringSplitOptions.RemoveEmptyEntries);
-        Student student = new Student();
-        Console.WriteLine(student.ToString());
+
+        List<Employees> students = new List<Employees >();
+
+        for (int i = 0; i < n; i++)
+        {
+            string[] studentInfo = Console.ReadLine().Split();
+            string firstName = studentInfo[0];
+            string lastName = studentInfo[1];
+            double grade = double.Parse(studentInfo[2]);
+
+            Employees student = new Employees(firstName, lastName, grade);
+            students.Add(student);
+        }
+
+        students.Sort((s1, s2) => s2.Salary.CompareTo(s1.Salary));
+
+        foreach (var student in students)
+        {
+            Console.WriteLine(student);
+        }
     }
 }
 
-class Student
+
+class Employees
 {
     public string FirstName { get; set; }
     public string LastName { get; set; }
-    public double Grade { get; set; }
-    class student
-    {
+    public double Salary { get; set; }
 
+    public Employees(string firstName, string lastName, double salary)
+    {
+        FirstName = firstName;
+        LastName = lastName;
+        Salary = salary;
     }
 
     public override string ToString()
     {
-        return $"{FirstName} {LastName} {Grade}";
+        return $"{FirstName} {LastName}: {Salary:F2}";
     }
 }
+
 
---
+++ class object/student/student/student.cs
@@ -0,0 +1,11 @@
+﻿using System;
+namespace student
+{
+	public class student
+	{
+		public student()
+		{
+		}
+	}
+}
+
