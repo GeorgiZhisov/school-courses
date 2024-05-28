526b04698ca11682ed6ec2263c4ca43d885fc860
GeorgiZhisov <ivojisov@gmail.com>
2024-05-28 16:31:16 +0300

    homework

--- exeption handling/enter numbers/enter numbers/Program.cs
+++ exeption handling/enter numbers/enter numbers/Program.cs
@@ -1,9 +1,42 @@
-﻿namespace enter_numbers;
-class Program
-{
-    static void Main(string[] args)
-    {
-        Console.WriteLine("Hello, World!");
-    }
-}
+﻿    int start = 1;
+    int end = 100;
 
+    List<int> numbers = new List<int>();
+
+    while (numbers.Count < 10)
+    {
+        start = ReadNumber(start, end);
+
+        if (start > 1)
+        {
+            numbers.Add(start);
+        }
+    }
+
+    Console.WriteLine(string.Join(", ", numbers));
+
+
+    int ReadNumber(int start, int end)
+    {
+        try
+        {
+            int number = int.Parse(Console.ReadLine());
+
+            if (number <= start || number >= end)
+            {
+                throw new Exception($"Your number is not in range {start} - {end}!");
+            }
+
+            return number;
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine("Invalid Number!");
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.Message);
+        }
+
+        return 1;
+    }
---
+++ exeption handling/sum of integers/sum of integers/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+
+class Program
+{
+    static void Main()
+    {
+        string input = Console.ReadLine();
+        string[] elements = input.Split(' ');
+
+        long sum = 0;
+
+        foreach (string element in elements)
+        {
+            sum = ProcessElement(element, sum);
+        }
+
+        Console.WriteLine($"The total sum of all integers is: {sum}");
+    }
+
+    static long ProcessElement(string element, long currentSum)
+    {
+        try
+        {
+            int number = int.Parse(element);
+            currentSum += number;
+        }
+        catch (FormatException)
+        {
+            Console.WriteLine($"The element '{element}' is in wrong format!");
+        }
+        catch (OverflowException)
+        {
+            Console.WriteLine($"The element '{element}' is out of range!");
+        }
+
+        Console.WriteLine($"Element '{element}' processed - current sum: {currentSum}");
+        return currentSum;
+    }
+}
